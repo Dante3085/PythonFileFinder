675d7915efbc4261c498357c0257a62cec7e12e2
mjsch <moritz97.s@web.de>
2020-09-18 10:36:02 +0200

    Don't open search_item with default app after it has been found.

--- fdfinder.py
+++ fdfinder.py
@@ -3,6 +3,12 @@ import sys
 import subprocess
 import itertools
 from pathlib import Path
+'''
+TODO:
+1. Make search_item and starting_directories fuzzy.
+   (Similar strings will also be searched)
+'''
+
 
 search_item = sys.argv[1]
 starting_directories = sys.argv[2:]
@@ -10,4 +16,4 @@ starting_directories = sys.argv[2:]
 for starting_directory in starting_directories:
     paths = list(Path(starting_directory).rglob(f"*{search_item}*"))
     if len(paths) > 0:
-        subprocess.Popen(f'explorer /open, {paths[0].absolute()}')
+        subprocess.Popen(f'explorer /select, {paths[0].absolute()}')
